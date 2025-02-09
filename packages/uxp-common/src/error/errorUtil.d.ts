db8dc52dda977cef146e1e07082ef912c1bb3d89
Fredrik Isaksson <freddi.isaksson@gmail.com>
2025-02-09 15:26:19 +0200

    refactor: remove unused type definitions and utility functions from uxp-common

--- packages/uxp-common/src/error/errorUtil.d.ts
+++
@@ -1,3 +0,0 @@
-import { ApiErrorResponse } from "./error.types";
-export declare const normalizeError: (error: unknown) => ApiErrorResponse;
-//# sourceMappingURL=errorUtil.d.ts.map
--- packages/uxp-common/src/general.types.d.ts
+++
@@ -1,4 +0,0 @@
-export type DeepPartial<T> = {
-    [P in keyof T]?: T[P] extends Array<infer U> ? Array<DeepPartial<U>> : T[P] extends object ? DeepPartial<T[P]> : T[P];
-};
-//# sourceMappingURL=general.types.d.ts.map
