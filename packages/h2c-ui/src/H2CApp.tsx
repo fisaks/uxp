d2f2446cd8d47590b2432eedb0754713ca9b1cb8
Fredrik Isaksson <freddi.isaksson@gmail.com>
2025-04-05 14:42:37 +0300

    fix: remove unnecessary whitespace in H2CApp component

--- packages/h2c-ui/src/H2CApp.tsx
+++ packages/h2c-ui/src/H2CApp.tsx
@@ -14,7 +14,6 @@ const H2CApp: React.FC = () => {
     return (
         <WebSocketConfig>
             <UxpTheme>
-
                 <RemoteAppListener />
 
                 <BrowserRouter basename={basePath}>
